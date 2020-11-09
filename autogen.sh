2df7299aff29affba7b3460b45238183d798bd36
Yogaraj Alamenda <yogarajx.alamenda@intel.com>
2020-11-09 07:35:32 +0000

    Refactor autotools build files from tools dir
    Signed-off-by: Yogaraj Alamenda <yogarajx.alamenda@intel.com>

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# Change directory to .tools as this script is location dependent
-
-tools_dir=$(cd `dirname $0` && pwd)/.tools
-cd $tools_dir
-
 if [ ! -d "m4" ]
 then
     mkdir m4
@@ -12,6 +7,4 @@ fi
 
 autoreconf -ivf
 
-rm -f ../configure
-sed 's/^srcdir\=$/srcdir\=\"\.tools\"/g' < $tools_dir/configure > ../configure
-chmod +x ../configure
+chmod +x configure
