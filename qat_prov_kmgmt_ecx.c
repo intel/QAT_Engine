8f5abba69202f5fea426f713e882494c253a3046
Venkatesh J <venkatesh.j@intel.com>
2025-09-11 16:20:05 -0400

    Add export and import routines for X448 provider.
    This would allow the export and import of X448 keys to other providers
    like default and oqsprovider.
    
    Signed-off-by: Venkatesh J <venkatesh.j@intel.com>

--- qat_prov_kmgmt_ecx.c
+++ qat_prov_kmgmt_ecx.c
@@ -447,15 +447,6 @@ static int qat_ecx_import(void *keydata, int selection, const OSSL_PARAM params[
     return fun(keydata, selection, params);
 }
 
-static const OSSL_PARAM* qat_ecx_import_types(int selection)
-{
-    typedef const OSSL_PARAM* (*fun_ptr)(int selection);
-    fun_ptr fun = get_default_x25519_keymgmt().import_types;
-    if (!fun)
-        return NULL;
-    return fun(selection);
-}
-
 static int qat_ecx_export(void *keydata, int selection, OSSL_CALLBACK *param_cb,
                           void *cbarg)
 {
@@ -497,7 +488,7 @@ static const OSSL_PARAM qat_ecx_key_types[] = {
     OSSL_PARAM_END
 };
 
-static const OSSL_PARAM *qat_ecx_export_types(int selection)
+static const OSSL_PARAM *qat_ecx_imexport_types(int selection)
 {
     if ((selection & OSSL_KEYMGMT_SELECT_KEYPAIR) != 0)
         return qat_ecx_key_types;
@@ -573,9 +564,9 @@ const OSSL_DISPATCH qat_X25519_keymgmt_functions[] = {
     { OSSL_FUNC_KEYMGMT_GEN_SETTABLE_PARAMS,
         (void (*)(void))qat_ecx_gen_settable_params },
     { OSSL_FUNC_KEYMGMT_IMPORT, (void (*)(void))qat_ecx_import },
-    { OSSL_FUNC_KEYMGMT_IMPORT_TYPES, (void (*)(void))qat_ecx_import_types },
+    { OSSL_FUNC_KEYMGMT_IMPORT_TYPES, (void (*)(void))qat_ecx_imexport_types },
     { OSSL_FUNC_KEYMGMT_EXPORT, (void (*)(void))qat_ecx_export },
-    { OSSL_FUNC_KEYMGMT_EXPORT_TYPES, (void (*)(void))qat_ecx_export_types },
+    { OSSL_FUNC_KEYMGMT_EXPORT_TYPES, (void (*)(void))qat_ecx_imexport_types },
     { OSSL_FUNC_KEYMGMT_DUP, (void (*)(void))qat_ecx_dup },
     { 0, NULL }};
 #endif
@@ -596,6 +587,10 @@ const OSSL_DISPATCH qat_X448_keymgmt_functions[] = {
     { OSSL_FUNC_KEYMGMT_GEN_SET_PARAMS, (void (*)(void))qat_ecx_gen_set_params },
     { OSSL_FUNC_KEYMGMT_GEN_SETTABLE_PARAMS,
         (void (*)(void))qat_ecx_gen_settable_params },
+    { OSSL_FUNC_KEYMGMT_IMPORT, (void (*)(void))qat_ecx_import },
+    { OSSL_FUNC_KEYMGMT_IMPORT_TYPES, (void (*)(void))qat_ecx_imexport_types },
+    { OSSL_FUNC_KEYMGMT_EXPORT, (void (*)(void))qat_ecx_export },
+    { OSSL_FUNC_KEYMGMT_EXPORT_TYPES, (void (*)(void))qat_ecx_imexport_types },
     { OSSL_FUNC_KEYMGMT_DUP, (void (*)(void))qat_ecx_dup },
     { 0, NULL }};
 #endif
